d355ceb37cd8cf6c31ea0c7b8c4620144228a13d
James Constance <jamessconstance@gmail.com>
2021-02-21 18:33:00 +1100

    Implementing GroceryList manipulation methods.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         GroceryList myGroceries = new GroceryList();
+
+        addGroceryItem(myGroceries);
+        addGroceryItem(myGroceries);
+        addGroceryItem(myGroceries);
+
+        myGroceries.printList();
+
+        replaceGroceryItem(myGroceries);
+
+        myGroceries.printList();
+
+        removeGroceryItem(myGroceries);
+
+        myGroceries.printList();
+
+        containsItem(myGroceries);
     }
 
     public static void addGroceryItem(GroceryList groceryList) {
