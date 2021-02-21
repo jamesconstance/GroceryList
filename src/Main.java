e221f06804d08ea3e340b89c8f952b268b0366c7
James Constance <jamessconstance@gmail.com>
2021-02-21 18:29:27 +1100

    Adding .gitignore file

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         GroceryList myGroceries = new GroceryList();
-
-
     }
 
     public static void addGroceryItem(GroceryList groceryList) {
